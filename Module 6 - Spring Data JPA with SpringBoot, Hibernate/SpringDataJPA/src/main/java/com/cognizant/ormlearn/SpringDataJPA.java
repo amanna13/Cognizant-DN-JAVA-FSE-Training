87ffff4b61062c8fbd2870d1ad6677efb2c39ef0
Ambarish Manna <amanna1041@gmail.com>
2026-07-08 11:46:25 +0530

    (module 6): setting up spring data jpa and mysql

---
+++ Module 6 - Spring Data JPA with SpringBoot, Hibernate/SpringDataJPA/src/main/java/com/cognizant/ormlearn/SpringDataJPA.java
@@ -0,0 +1,32 @@
+package com.cognizant.ormlearn;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+@SpringBootApplication
+public class SpringDataJPA {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpringDataJPA.class);
+    private static CountryService countryService;
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(SpringDataJPA.class, args);
+        countryService = applicationContext.getBean(CountryService.class);
+        logger.info("Inside Main");
+
+        testAllCountries();
+    }
+    private static void testAllCountries() {
+        logger.info("Start");
+        List<Country> countries = countryService.getAllCountries();
+        logger.debug("counties = {}", countries);
+        logger.info("End");
+    }
+}
