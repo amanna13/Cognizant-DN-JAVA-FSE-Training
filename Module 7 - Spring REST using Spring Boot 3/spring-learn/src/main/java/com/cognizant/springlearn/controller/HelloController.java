49df1402a20cc008fa2647e05373635072b1e012
Ambarish Manna <amanna1041@gmail.com>
2026-07-17 14:17:43 +0530

    (module 7): implemented hello world restful web service

---
+++ Module 7 - Spring REST using Spring Boot 3/spring-learn/src/main/java/com/cognizant/springlearn/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.cognizant.springlearn.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> sayHello() {
+        logger.info("Start");
+        return ResponseEntity.status(HttpStatus.OK).body("Hello World !");
+    }
+}
