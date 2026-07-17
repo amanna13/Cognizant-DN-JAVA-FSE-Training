ced2962b8b5a611363ceec7e33a791c7839d7fad
Ambarish Manna <amanna1041@gmail.com>
2026-07-17 12:08:14 +0530

    (module 7): implemented spring core load Country from Spring Configuration XML

--- Module 7 - Spring REST using Spring Boot 3/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ Module 7 - Spring REST using Spring Boot 3/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -1,19 +1,32 @@
 package com.cognizant.springlearn;
 
+import com.cognizant.springlearn.model.Country;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
-    private static Logger logger = LoggerFactory.getLogger(SpringLearnApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(SpringLearnApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringLearnApplication.class, args);
 
         logger.info("Spring Web Project Started");
+
+        //Load Country from Spring Configuration XML
+        displayCountry();
+    }
+
+    public static void displayCountry() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("country.xml");
+        Country country = applicationContext.getBean("country", Country.class);
+
+        logger.debug("Country - {}", country.toString());
     }
 
 }
