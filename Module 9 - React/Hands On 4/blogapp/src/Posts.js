844fcd2a6370da17c6e15ec38d985ced8a188ad3
Ambarish Manna <amanna1041@gmail.com>
2026-07-29 01:12:17 +0530

    (module 9):  add lifecycle method lab instruction steps for blogapp"

---
+++ Module 9 - React/Hands On 4/blogapp/src/Posts.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Post from './Post';
+
+class Posts extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+    }
+
+    loadPosts() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => {
+                const posts = data.map(
+                    p => new Post(p.id, p.title, p.body)
+                );
+
+                this.setState({
+                    posts: posts
+                });
+            });
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.posts.map(post => (
+                        <div key={post.id}>
+                            <h3>{post.title}</h3>
+                            <p>{post.body}</p>
+                        </div>
+                    ))
+                }
+            </div>
+        );
+    }
+
+    componentDidCatch(error, info) {
+        alert(error);
+    }
+}
+
+export default Posts;
