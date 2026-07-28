fbee632f31208d54d2622fb451feb8015d829bd1
Ambarish Manna <amanna1041@gmail.com>
2026-07-29 01:10:06 +0530

    (module 9): add CalculateScore component to compute student averages

---
+++ Module 9 - React/Hands On 3/scorecalculator/src/App.js
@@ -0,0 +1,14 @@
+import {CalculateScore} from '../src/Components/CalculateScore';
+
+function App(){
+  return(
+    <div>
+      <CalculateScore Name={"Mabud"}
+      School={"DAV Public School"}
+      total={286}
+      goal={3}/>
+    </div>
+  )
+}
+
+export default App;
